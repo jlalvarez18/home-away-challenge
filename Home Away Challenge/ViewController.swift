ca72cb9b8770480d7587ff65375ba70ce1a52e57
Juan Alvarez <jlalvarez18@gmail.com>
2018-11-20 15:50:28 -0600

    Initial Commit

---
+++ Home Away Challenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Home Away Challenge
+//
+//  Created by Juan Alvarez on 11/20/18.
+//  Copyright © 2018 Juan Alvarez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
